92106b5d521d6381c5c002ce45e0f93e4b86982e
Jakub Kawecki <jakub@forloop.ai>
2023-10-30 14:04:26 +0100

    NEW: Reworked Trigger dataclass

--- forloop_common_structures/core/trigger.py
+++ forloop_common_structures/core/trigger.py
@@ -1,25 +1,13 @@
-from dataclasses import dataclass, field
-from typing import ClassVar, Any, Dict, List
 import datetime
+from dataclasses import dataclass
+from typing import Optional
+
 
 @dataclass
 class Trigger:
-    
-    trigger_name: str=""
-    machine_uid: int=""
-    pipeline_uid: int=""
-    first_run_datetime: datetime.datetime=datetime.datetime.now()
-    frequency: int=""
-    project_uid: str="0"
-    
-    
-    
-    
-    uid: int = field(init=False)
-    instance_counter: ClassVar[int] = 0
-
-
-    def __post_init__(self):
-        self.__class__.instance_counter += 1
-        self.uid = str(self.instance_counter)
-        
+    trigger_name: str
+    pipeline_uid: int
+    frequency: int
+    project_uid: str
+    first_run: datetime.datetime
+    uid: Optional[str] = None
