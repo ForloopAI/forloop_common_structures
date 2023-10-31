175777bbe42f3cd68e050bdc95f27ac41ff75acc
Jakub Kawecki <jakub@forloop.ai>
2023-10-31 10:05:37 +0100

    CHANGE: Replaced int frequency with enum in Trigger

--- forloop_common_structures/core/trigger.py
+++ forloop_common_structures/core/trigger.py
@@ -1,13 +1,20 @@
 import datetime
 from dataclasses import dataclass
+from enum import Enum
 from typing import Optional
 
 
+class TriggerFrequencyEnum(str, Enum):
+    HOURLY = "hourly"
+    DAILY = "daily"
+    WEEKLY = "weekly"
+    MONTHLY = "monthly"
+
 @dataclass
 class Trigger:
-    trigger_name: str
-    pipeline_uid: int
-    frequency: int
-    project_uid: str
+    name: str
     first_run: datetime.datetime
+    frequency: TriggerFrequencyEnum
+    pipeline_uid: int
+    project_uid: str
     uid: Optional[str] = None
