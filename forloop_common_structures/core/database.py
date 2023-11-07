90c71c74c8038fc10fb4532cb1b000cabcdea2b6
Jakub Kawecki <jakub@forloop.ai>
2023-11-07 14:41:14 +0100

    NEW: Added DbDialectEnum to database.py

--- forloop_common_structures/core/database.py
+++ forloop_common_structures/core/database.py
@@ -1,24 +1,29 @@
 from dataclasses import dataclass, field
-from typing import ClassVar, Any, Dict, List
+from enum import Enum
+from typing import ClassVar
+
+
+class DbDialectEnum(str, Enum):
+    MYSQL = "mysql"
+    POSTGRES = "postgres"
+    MONGO = "mongo"
 
 
 @dataclass
 class Database:
-    
+
     database_name: str
     server: str
     port: int
     database: str
     username: str
-    password: str    
+    password: str
     dialect: str
-    project_uid: int=0
-    
+    project_uid: int = 0
+
     uid: str = field(init=False)
     instance_counter: ClassVar[int] = 0
-
 
     def __post_init__(self):
         self.__class__.instance_counter += 1
         self.uid = str(self.instance_counter)
-        
