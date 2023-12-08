6578958d8a36f10dc61fcbc4812ba4e80cb93923
Jakub Kawecki <jakub@forloop.ai>
2023-12-08 15:19:38 +0100

    CHANGE: Replaced unnecessary Enums with Literals

--- forloop_common_structures/core/session.py
+++ forloop_common_structures/core/session.py
@@ -1,21 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
-from enum import Enum
-from typing import Optional
-
-
-class PlatformEnum(str, Enum):
-    """Simple enum listing options for platform_type attribute in Session dataclass/validation schema."""
-
-    CLOUD = "cloud"
-    DESKTOP = "desktop"
+from typing import Literal, Optional
 
 
 @dataclass
 class Session:
     user_uid: str
     auth0_session_id: str  #auth0 response - session_id
-    platform_type: PlatformEnum  #cloud or desktop
+    platform_type: Literal["cloud", "desktop"]  #cloud or desktop
     version: Optional[str] = None  #forloop platform version
     ip: Optional[str] = None  # only in desktop/execution core version
     mac_address: Optional[str] = None  # only in desktop/execution core version
