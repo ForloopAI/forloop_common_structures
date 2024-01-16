cc6f41a41b0ceffdcf33ca6c9cf64fa0be255fc6
Jakub Kawecki <jakub@forloop.ai>
2024-01-16 13:56:34 +0100

    NEW: Add OperationJob and PrototypeJob dataclasses

--- forloop_common_structures/core/job.py
+++ forloop_common_structures/core/job.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from enum import Enum
 from typing import Optional, TypedDict
 
-from forloop_common_structures.core.node import Node
 from forloop_common_structures.core.edge import Edge
+from forloop_common_structures.core.node import Node
 from forloop_common_structures.core.variable import Variable
 
 
@@ -28,11 +28,11 @@ class JobStatusEnum(str, Enum):
 
 @dataclass
 class NodeJob:
-    pipeline_uid: str
+    pipeline_uid: str  # TODO: Remove when PrototypeJobs are implemented
     node: Node
 
     uid: Optional[str] = None
-    machine_uid: Optional[str] = None
+    machine_uid: Optional[str] = None  # TODO: Remove when PrototypeJobs are implemented
     status: JobStatusEnum = JobStatusEnum.QUEUED
     created_at: datetime = field(default_factory=datetime.utcnow)
     started_at: Optional[datetime] = None
@@ -63,7 +63,7 @@ class PipelineElements(TypedDict):
 @dataclass
 class PipelineJob:
     pipeline_uid: str
-    pipeline_elements: PipelineElements
+    pipeline_elements: PipelineElements # TODO: Discuss and remove (?) when PrototypeJobs are implemented - current pipeline on the backend should be used
 
     uid: Optional[str] = None
     machine_uid: Optional[str] = None
@@ -81,6 +81,7 @@ class PipelineJob:
         # Deserialize a dict with nodes/edges/variables dicts
         self._deserialize_pipeline_elements()
 
+    # TODO: Discuss and remove (?) when PrototypeJobs are implemented - current pipeline on the backend should be used
     def _deserialize_pipeline_elements(self) -> None:
         """Deserialize nodes, edges, variables into list of objects in case the Job was instantiated from JSON."""
         name_mapping = {"nodes": Node, "edges": Edge, "variables": Variable}
@@ -95,6 +96,53 @@ class PipelineJob:
                 ]
 
         self.pipeline_elements = deserialized_elements
+
+    def __str__(self) -> str:
+        """Create string representation of the class used in logging."""
+        return f"{type(self).__name__}:{self.uid}"
+
+
+@dataclass
+class OperationJob:
+    prototype_job_uid: int
+    node: Node
+
+    uid: Optional[str] = None
+    machine_uid: Optional[str] = None
+    status: JobStatusEnum = JobStatusEnum.QUEUED
+    created_at: datetime = field(default_factory=datetime.utcnow)
+    started_at: Optional[datetime] = None
+    completed_at: Optional[datetime] = None
+    message: Optional[str] = None
+
+    def __post_init__(self):
+        # Deserialize dict into a Node if NodeJob is instantiated from a JSON
+        if isinstance(self.node, dict):
+            self.node = Node(**self.node)
+
+        # Deserialize into Enum if instantiated from JSON
+        if isinstance(self.status, str):
+            self.status = JobStatusEnum(self.status)
+
+    def __str__(self) -> str:
+        """Create string representation of the class used in logging."""
+        return f"{type(self).__name__}:{self.uid}"
+
+
+@dataclass
+class PrototypeJob:
+    pipeline_uid: str
+    machine_uid: Optional[str] = None
+    uid: Optional[str] = None
+    status: JobStatusEnum = JobStatusEnum.QUEUED
+    created_at: datetime = field(default_factory=datetime.utcnow)
+    started_at: Optional[datetime] = None
+    completed_at: Optional[datetime] = None
+
+    def __post_init__(self) -> None:
+        # Deserialize into Enum if instantiated from JSON
+        if isinstance(self.status, str):
+            self.status = JobStatusEnum(self.status)
 
     def __str__(self) -> str:
         """Create string representation of the class used in logging."""
