73c7c61c8bae6f497dbb32b6058e9bf1d358fb2a
Dominik Vach <vach.dominik@gmail.com>
2024-01-19 18:16:23 +0100

    release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='forloop_common_structures',
-    version='0.1.9',
+    version='0.2.0',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='This package contains open source core structures and schemas within Forloop.ai execution core and API',
