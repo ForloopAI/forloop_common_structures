bb294c39ba4961ad88d9318b3924333dc69782cd
Jakub Kawecki <jakub@forloop.ai>
2024-06-21 11:51:31 +0200

    Add `SubscriptionPlan` and `UserSubscriptionPlan` dataclasses

---
+++ forloop_common_structures/core/plan.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass
+class SubscriptionPlan:
+    name: str
+    system_unique_name: str
+    price: float
+    total_credits: int
+    max_concurrent_pipelines: int
+    max_collaborators: int
+    description: str
+    uid: Optional[str] = None
+
+@dataclass
+class UserSubscriptionPlan:
+    user_uid: str # Foreign Key Many-to-1
+    subscription_plan_uid: str # Foreign Key Many-to-1
+    end_datetime_utc: datetime
+    consumed_credits: int = 0
+    # total_credits: int
+    start_datetime_utc: datetime = field(default_factory=datetime.utcnow)
+    uid: Optional[str] = None
+
