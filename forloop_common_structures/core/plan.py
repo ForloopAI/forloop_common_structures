718a1962d80b4dd48d88e1877395f3a19512bc7c
Jakub Kawecki <jakub@forloop.ai>
2024-07-01 14:42:45 +0200

    Removed `UserSubscriptionPlanPaymentStatusEnum`

--- forloop_common_structures/core/plan.py
+++ forloop_common_structures/core/plan.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
 
-from forloop_modules.queries.db_model_templates import UserSubscriptionPlanPaymentStatusEnum
-
 
 @dataclass
 class SubscriptionPlan:
@@ -22,9 +20,8 @@ class UserSubscriptionPlan:
     user_uid: str # Foreign Key Many-to-1
     subscription_plan_uid: str # Foreign Key Many-to-1
     end_datetime_utc: datetime
-    consumed_credits: int = 0
     # total_credits: int
-    status: UserSubscriptionPlanPaymentStatusEnum
+    consumed_credits: int = 0
     start_datetime_utc: datetime = field(default_factory=datetime.utcnow)
     uid: Optional[str] = None
 
