7fa42d2ecca2f86f50dbf54c469bee11b0dd6f59
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-09-26 11:11:06 +0200

    Improva variable and initial_variable size evaluation

--- forloop_common_structures/core/initial_variable.py
+++ forloop_common_structures/core/initial_variable.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from numbers import Number
 from typing import Optional, Union
 
+import numpy as np
 import pandas as pd
 
 VariableValueTypes = Union[str, Number, pd.DataFrame, list, dict]
@@ -23,13 +24,17 @@ class InitialVariable:
     def __post_init__(self) -> None:
         if self.type is None:
             self.type = type(self.value).__name__
-            self.size = 0
 
         if self.size is None:
-            try:
-                self.size = len(self.value)
-            except TypeError:
-                self.size = 1
+            if self.value is None:
+                self.size = 0
+            elif isinstance(self.value, (pd.DataFrame, pd.Series, np.ndarray)):
+                self.size = self.value.size
+            else:
+                try:
+                    self.size = len(self.value)
+                except TypeError:
+                    self.size = 1
 
     def __str__(self) -> str:
         return f"{self.value}"
